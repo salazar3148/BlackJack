37198822b102e64b406ff54e4ad8a6f4d6156f5a
salazar3148 <salazar3148@gmail.com>
2022-12-17 13:44:17 -0500

    Done

--- assets/js/start.js
+++ assets/js/start.js
@@ -1,5 +1,20 @@
+//HTML REFERENCES
 
-let player1 = [];
+const btnGetCard = document.querySelector("#btnGetCard");
+const btnStop = document.querySelector("#btnStop");
+const btnNewGame = document.querySelector("#btnNewGame");
+const scoreboard = document.querySelectorAll("#score");
+const playerCards = document.querySelectorAll("#player-cards"); 
+
+//Logic Game
+
+let player1 = {name: "Player1", score: 0, pos: 0};
+let player2 = {name: "Player2", score: 0, pos: 1};
+btnNewGame.disabled = true;
+
+//Player in Game
+let inGame = player1;
+
 let types = ["C", "D", "H", "D"];
 let others = ["A", "J", "Q", "K"];
 
@@ -20,7 +35,15 @@ const newDeck = () => {
     return newDeck;
 }
 
-//const getCard = deck => deck.pop(); 
+const disabledButtons = (status) => {
+    btnGetCard.disabled = status;
+    btnStop.disabled = status;
+    btnNewGame.disabled = !status;
+}
+
+deck = newDeck();
+
+const getCard = () => deck.pop(); 
 
 const valueA = () => {
     let A;
@@ -36,9 +59,52 @@ const valueCard = card => {
     return parseInt(card[0]);
 };
 
+const setScore = (player, newScore) => {
+    player.score = newScore;
+    scoreboard[player.pos].innerText = player.score;
+}
 
-player1 = newDeck();
-card = getCard(player1);
-console.log(valueCard(card));
+const winmessage = () => {
+    if(player1.score == player2.score) (alert("Draw!"))
+    player1.score > player2.score ? alert("Player 1 Wins!"): alert("Player2 Wins!");
+}
 
-//EVENTOS
+//EVENTS
+btnNewGame.addEventListener("click", () => {
+    deck = newDeck();
+    btnNewGame.disabled = true;
+    disabledButtons(false);
+    setScore(player1, 0);
+    setScore(player2, 0);
+    inGame = player1;
+    playerCards[0].innerHTML = "";
+    playerCards[1].innerHTML = "";
+})
+
+btnGetCard.addEventListener("click", () => {
+    const card = getCard();
+    const newScore = valueCard(card);;
+    setScore(inGame, newScore + inGame.score);
+    const imgCard = document.createElement("img");
+    imgCard.classList.add("card")
+    imgCard.src = `assets/cards/${card}.png`;
+    playerCards[inGame['pos']].append(imgCard);
+
+    if(inGame.score > 21){
+        alert(`${inGame.name} missing!`)
+        disabledButtons(true);
+    }
+})
+
+btnStop.addEventListener("click", () => {
+    if(inGame == player2) {
+        if(player2.score < player1.score) alert("Your score must be higher!");
+        else {
+            winmessage();
+            disabledButtons(true);
+        }
+    } else {
+        alert("it's time Player 2!")
+        inGame = player2;
+    }
+})
